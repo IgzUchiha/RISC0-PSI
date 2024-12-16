978b1ae53a7f0dcbf86e27c771e3a4690613c0fc
IgzUchiha <iamsupervegeta14@gmail.com>
2024-12-16 12:21:53 -0800

    Journal Progress

--- methods/guest/src/bin/journal.rs
+++ methods/guest/src/bin/journal.rs
@@ -1,16 +1,24 @@
-use risc0_steel::SolCommitment;
+use alloy_sol_types::{sol, SolValue};
+use risc0_zkvm::guest::env;
+use alloy_primitives::Address;
 
-sol! {
-    struct Journal {
-        SolCommitment commitment;
-        address tokenAddress;
-    }
+// sol! {
+//     struct Journal {
+//         commitment: BlockCommitment, // Ensure BlockCommitment is SolType-compatible
+//         tokenAddress: Address,
+//     }
+// }
+pub struct Journal {
+    pub bytes: Vec<u8>,
 }
 
+fn main() {
+    let token_address = Address::from_str("0x1234567890abcdef1234567890abcdef12345678").unwrap();
+    let journal = Journal {
+        commitment: BlockCommitment::default(),
+        tokenAddress: token_address,
+    };
 
-
-let journal = Journal {
-    commitment: view_call_env.block_commitment(),
-    tokenAddress,
-};
-env::commit_slice(&journal.abi_encode());
+    // Encode and commit the journal
+    env::commit_slice(&journal.abi_encode());
+}
